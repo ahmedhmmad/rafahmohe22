8d5c7f3a195ceb686b948bf3524e43bea0aebe2a
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2023-01-28 13:18:43 +0200

    ads

---
+++ database/migrations/2023_01_28_103123_create_ads_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->id();
+            $table->string('ads_writer');
+            $table->string('ads_department');
+            $table->string('ads_title');
+            $table->text('ads_description');
+            $table->string('ads_main_image');
+            $table->string('ads_second_image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ads');
+    }
+};
