3493e9b9bb23596ebca7ac8d1c41d4fcabdb3c19
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2023-01-29 21:30:45 +0200

    contact us

--- app/Http/Controllers/Admin/HomePageController.php
+++ app/Http/Controllers/Admin/HomePageController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\HomePage;
 use Illuminate\Http\Request;
 
 class HomePageController extends Controller
 {
     //
+    public function welcome()
+    {
+       echo 'Welcome';
+        return HomePage::all();
+    }
 }
