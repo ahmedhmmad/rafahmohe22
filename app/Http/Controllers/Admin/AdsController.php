0d931fb9fec683f1735134a27b7b4bf17a21face
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2023-01-29 22:07:19 +0200

    add ads,social controllers and routes

--- app/Http/Controllers/Admin/AdsController.php
+++ app/Http/Controllers/Admin/AdsController.php
@@ -3,9 +3,31 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Ads;
 use Illuminate\Http\Request;
 
 class AdsController extends Controller
 {
     //
+    public function viewAds()
+    {
+        return Ads::all();
+    }
+
+    public function addAds(Request $request)
+    {
+        $result = Ads::insert(
+            [
+                'ads_writer' => 'admin',
+                'ads_department' => $request->department,
+                'ads_title' => $request->title,
+                'ads_description' => $request->descrption,
+                'ads_main_image' => $request->main_image,
+                'ads_second_image' => $request->second_image,
+
+            ]
+        );
+        if ($result) return 1;
+        else return 0;
+    }
 }
